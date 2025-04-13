45ff25a6ca095799767aafdab9aae7fa4ffc8cd2
Dhara251709 <dharaparmar2509@gmail.com>
2025-04-13 22:31:24 +0530

    Add files via upload

---
+++ even odd number cheker.py
@@ -0,0 +1,6 @@
+print("Here you can check wherever number is even or odd?")
+number_to_check = int(input("What is the number you want to check?"))
+if number_to_check % 2 == 0:
+    print("This no is Even.")
+else:
+    print("This no is Odd.")
